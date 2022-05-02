3debabb163981c96c1868aa839fb41135c3f321a
kheira05 <81557099+kheira05@users.noreply.github.com>
2022-05-03 00:10:55 +0200

    page détails + customisation drapeau

--- projet/ViewModels/CountryPageViewModel.cs
+++ projet/ViewModels/CountryPageViewModel.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows.Input;
 using projet.Service;
+using Xamarin.Essentials;
+using Xamarin.Forms;
 
 namespace projet.ViewModels
 {
@@ -12,10 +17,54 @@ namespace projet.ViewModels
             set { SetProperty(ref name, value); }
         }
 
+        Uri flags;
+        public Uri Flags
+        {
+            get { return flags; }
+            set { SetProperty(ref flags, value); }
+        }
+
+        string capital;
+        public string Capital
+        {
+            get { return capital; }
+            set { SetProperty(ref capital, value); }
+        }
+
+        string population;
+        public string Population
+        {
+            get { return population; }
+            set { SetProperty(ref population, value); }
+        }
+
+        string currencies;
+        public string Currencies
+        {
+            get { return currencies; }
+            set { SetProperty(ref currencies, value); }
+        }
 
         public CountryPageViewModel(Country c)
         {
             Name = c.Name.Common;
+            Flags = c.Flags.Png;
+            Capital = c.Capital[0];
+            Population = c.Population.ToString("#,#", CultureInfo.InvariantCulture);
+            Currencies = c.Currencies.Byn.Name;
+        }
+        public ICommand OnButtonClickedCommand
+        {
+            get
+            {
+                return new Command(ButtonClicked);
+            }
+        }
+        void ButtonClicked()
+        {
+
+            /*Application.Current.MainPage.DisplayAlert("wiki", "https://en.wikipedia.org/wiki/"+Name, "Ok");*/
+            Browser.OpenAsync("https://en.wikipedia.org/wiki/" + Name);
         }
     }
 }
---
+++ projet/Views/RoundedImage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace projet.Views
+{
+    public partial class RoundedImage : ContentView
+    {
+        public RoundedImage()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty ImageProperty = BindableProperty.Create(nameof(Image), typeof(ImageSource),
+            typeof(RoundedImage), null, propertyChanged: (bindable, oldValue, newvalue)
+            =>
+            {
+                if (newvalue is ImageSource image && bindable is RoundedImage)
+                {
+                    ((RoundedImage)bindable).image.Source = (ImageSource)newvalue;
+                }
+            });
+
+        public static readonly BindableProperty SizeProperty = BindableProperty.Create(nameof(Size), typeof(Double),
+            typeof(RoundedImage), null, propertyChanged: (bindable, oldValue, newvalue)
+            =>
+            {
+                if (newvalue is Double frame && bindable is RoundedImage)
+                {
+                    ((RoundedImage)bindable).frame.HeightRequest = (float)(Double)newvalue;
+                    ((RoundedImage)bindable).frame.WidthRequest = (float)(Double)newvalue;
+                    ((RoundedImage)bindable).frame.CornerRadius = (float)(Double)newvalue / 2;
+
+                }
+            });
+
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        public Double s
+        {
+            get { return (Double)GetValue(SizeProperty); }
+            set { SetValue(SizeProperty, value); }
+        }
+    }
+}
